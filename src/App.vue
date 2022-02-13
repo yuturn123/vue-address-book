48386685877bee404c8ee40857a48734c77a7071
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-02-13 23:54:12 +0900

    sample

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
         <h5>sample</h5>
+        <h6>sample2</h6>
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
