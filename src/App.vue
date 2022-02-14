6c1de4465e461de79b87a43caf7a6f134317f7b6
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-02-14 23:31:04 +0900

    sample

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@
       <v-toolbar-side-icon @click="toggleSideMenu"></v-toolbar-side-icon>
       <v-toolbar-title class="headline text-uppercase">
         <span>マイアドレス帳</span>
-        <h5>sample</h5>
-        <h6>sample2</h6>
+
       </v-toolbar-title>
       <v-spacer></v-spacer>
     </v-toolbar>
     <SideNav />
-    <Hello />
+
 
     <v-content>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
@@ -20,12 +20,12 @@
 <script>
 import { mapActions } from 'vuex';
 import SideNav from  './components/SideNav';
-import Hello from './components/HelloWorld';
+//import Hello from './components/HelloWorld';
 export default {
   name: 'App',
   components: {
     SideNav,
-    Hello,
+
   },
   data () {
     return {
