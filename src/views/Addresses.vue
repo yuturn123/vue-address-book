a06a5f2edcbb1eecb4a6b27959869945a2224830
yuturn123 <haha.huhu.8yuta@icloud.com>
2022-02-16 00:44:56 +0900

    連絡先追加画面の作成

--- src/views/Addresses.vue
+++ src/views/Addresses.vue
@@ -6,6 +6,14 @@
       </v-flex>
 
       <v-flex xs12 mt-5 justify-center>
+        <router-link :to="{name: 'address_edit'}">
+          <v-btn color="info">
+            連絡先追加
+          </v-btn>
+        </router-link>
+      </v-flex>
+
+      <v-flex xs12 mt-5 text-xs-right>
         <v-data-table :headers='headers' :items='addresses'>
           <template v-slot:items="props">
             <td class="text-xs-left">{{ props.item.name }}</td>
